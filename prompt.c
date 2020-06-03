8ffa9d8dd88c9ef860a28cdde2d5b4eb0d284c4a
LuisJackRamirez <l_armando_2000@hotmail.com>
2020-06-03 15:08:10 -0500

    Estandarización de los errores, añadidos comentarios del código.

--- prompt.c
+++ prompt.c
@@ -21,27 +21,38 @@ main (void)
 }
 */
 
+
+//Funcion que obtiene el prompt del shell.
+//El prompt tiene soporte para 256 caracteres.
 void
 getPrompt (char *str)
 {
   char buffer[256];
 
+  //Usuario del equipo
   getOutput (buffer, "/usr/bin/whoami");
   strcpy (str, buffer);
 
+  //Caracter A
   strcat (str, " @ ");
 
+  //Nombre del equipo
   getOutput (buffer, "/bin/hostname");
   strcat (str, buffer);
 
+  //Caracter B
   strcat (str, " : ");
 
+  //Directorio de trabajo actual
   getOutput (buffer, "/bin/pwd");
   strcat (str, buffer);
 
+  //Caracter C
   strcat (str, " > ");
 }
 
+//Función que obtiene el resultado
+//del comando pasado como argumento
 void
 getOutput (char *str, char *command)
 {
@@ -49,17 +60,21 @@ getOutput (char *str, char *command)
   char buffer[256];
   char *aux;
 
+  //popen () invoca al shell mediante una tubería.
+  //indicando el comando y el tipo de comunicación.
   fp = popen (command, "r");
 
   if (fp == NULL)
     {
-      perror ("Error en popen");
+      perror ("\n\tError en popen: getOutput (), prompt.c en línea 56 :");
       exit (-1);
     }
 
   if (fgets (buffer, sizeof (buffer), fp) != NULL)
     strcpy (str, buffer);
 
+  //Eliminación del salto de línea concatenado
+  //por la función fgets
   aux = strchr (str, '\n');
   if (aux != NULL)
     *aux = '\0';
--- redirection.c
+++ redirection.c
@@ -32,23 +32,31 @@ main (void)
 }
 */
 
+//Función que ejecuta la redirección de
+//entrada con sobreescritura o concatenación, 
+//y la redirección de salida.
 void
 executeRedirection (char *input, char *output, int mode)
 {
   int fileDes = 0;
   int pid = 0;
 
+  //Obtención del conjunto de cadenas de argumentos
   char **inArgs;
   inArgs = getArgs (input);
 
-  if (mode == 1)      
-    fileDes = open (output, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+  //Obtención del descriptor de archivo para el argumento output
+  //  1 - Sobreescritura
+  //  2 - Concatenación
+  if (mode == 1)
+    fileDes = open (output, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
   else if (mode == 2)
-    fileDes = open (output, O_CREAT|O_WRONLY|O_APPEND, S_IRWXU);
+    fileDes = open (output, O_CREAT | O_WRONLY | O_APPEND, S_IRWXU);
 
   if (fileDes == -1)
     {
-      perror ("\nError en apertura de archivo\n");
+      perror
+	("\n\tError en open: executeRedirection (), redirection.c en línea 48/50 :");
       exit (-1);
     }
 
@@ -56,17 +64,22 @@ executeRedirection (char *input, char *output, int mode)
 
   if (pid == -1)
     {
-      perror ("\nError en fork ()\n");
+      perror
+	("\n\tError en fork: executeRedirection (), redirection.c en línea 59 :");
       exit (-1);
     }
   else if (pid == 0)
     {
+      //Se cierra la salida estándar, es reemplazada
+      //por el descriptor de archivo obtenido previamente.
+
       //dup2 (fileDes, fileno (stdout));  
       close (1);
       dup (fileDes);
 
       execvp (inArgs[0], inArgs);
-      perror ("\nError en exec ()\n");
+      perror
+	("\n\tError en execvp: executeRedirection (), redirection.c en línea 72 :");
     }
   else
     {
@@ -74,6 +87,8 @@ executeRedirection (char *input, char *output, int mode)
       wait (NULL);
     }
 
+  //Se libera el espacio de memoria ocupado
+  //por el conjunto de cadenas de argumentos
   free (inArgs);
 
   return;
@@ -85,6 +100,7 @@ executeRedirectionOut (char *input, char *output)
   int fileDes = 0;
   int pid = 0;
 
+  //obtención del conjunto de cadenas de argumentos
   char **inArgs;
   inArgs = getArgs (output);
 
@@ -92,7 +108,8 @@ executeRedirectionOut (char *input, char *output)
 
   if (fileDes == -1)
     {
-      perror ("\nError en apertura de archivo\n");
+      perror
+	("\n\tError en open: executeRedirectionOut (), redirection.c en línea 95 :");
       exit (-1);
     }
 
@@ -100,17 +117,22 @@ executeRedirectionOut (char *input, char *output)
 
   if (pid == -1)
     {
-      perror ("\nError en fork ()\n");
+      perror
+	("\n\tError en fork: executeRedirectionOut (), redirection.c en línea 106 :");
       exit (-1);
     }
   else if (pid == 0)
     {
+      //Se cierra la salida estándar, es reemplazada
+      //por el descriptor de archivo obtenido previamente.
+
       //dup2 (fileDes, fileno (stdout));  
       close (0);
       dup (fileDes);
 
       execvp (inArgs[0], inArgs);
-      perror ("\nError en exec ()\n");
+      perror
+	("\n\tError en execvp: executeRedirectionOut (), redirection.c en línea 120 :");
     }
   else
     {
@@ -118,6 +140,8 @@ executeRedirectionOut (char *input, char *output)
       wait (NULL);
     }
 
+  //Se libera el espacio de memoria ocupado
+  //por el conjunto de cadenas de argumentos
   free (inArgs);
 
   return;
